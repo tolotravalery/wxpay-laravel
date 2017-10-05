d410a2415a08fe0413b70b3679772f9e393e6c21
koera-sam <tolotrarazafindrabe@gmail.com>
2017-10-05 15:32:41 +0200

    Integrate to laravel

--- app/Http/Controllers/PayementController.php
+++ app/Http/Controllers/PayementController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\NativePay;
 
 class PayementController extends Controller
 {
@@ -13,7 +14,11 @@ class PayementController extends Controller
         $renderer->setWidth(256);
         $writer = new \BaconQrCode\Writer($renderer);
         $image_name = strtotime('now');
-        $writer->writeFile('Hello World!', 'public/qr_code/' . $image_name . '.png');
+        $notify = new NativePay();
+        $url1 = $notify->GetPrePayUrl("123456789");
+//        dd(urlencode($url1));
+        $writer->writeFile($url1, 'public/qr_code/' . $image_name . '.png');
         return view('qr-code')->with('image', $image_name);
+
     }
 }
--- resources/views/qr-code.blade.php
+++ resources/views/qr-code.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="token" content="{{csrf_token()}}"/>
     <link href="{{url('/')}}/public/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{url('/')}}/public/css/style.css">
     <link rel="stylesheet" href="{{url('/')}}/public/css/mediaqueries.css">
